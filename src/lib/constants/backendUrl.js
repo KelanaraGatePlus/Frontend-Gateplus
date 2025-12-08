109baea019d1152124cecfe94a91f6bc71f49a9d
Muhammad Rakha Nasjaya <muhammadrakha3704@gmail.com>
2025-12-08 17:42:57 +0700

    [FIX] - Backend URL from env

--- src/lib/constants/backendUrl.js
+++ src/lib/constants/backendUrl.js
@@ -1,2 +1,4 @@
+// eslint-disable-next-line no-undef
 export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3000";
+// eslint-disable-next-line no-undef
 export const MIDTRANS_URL = process.env.MIDTRANS_URL || "https://app.sandbox.midtrans.com/snap/snap.js";
