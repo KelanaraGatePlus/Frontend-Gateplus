9da3e486947cd26405b3fa41df30dc5efbf49f69
Muhammad Rakha Nasjaya <muhammadrakha3704@gmail.com>
2025-12-08 17:39:13 +0700

    [FIX] - Backend URL

--- src/lib/constants/backendUrl.js
+++ src/lib/constants/backendUrl.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:3000";
-export const MIDTRANS_URL = "https://app.sandbox.midtrans.com/snap/snap.js";
+export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3000";
+export const MIDTRANS_URL = process.env.MIDTRANS_URL || "https://app.sandbox.midtrans.com/snap/snap.js";
