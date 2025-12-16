001093286350d054a8cf00503d57189ba83c76ae
Muhammad Rakha Nasjaya <muhammadrakha3704@gmail.com>
2025-12-16 12:01:52 +0700

    [FIX] - Bug fix on getting backend URL from env

--- src/lib/constants/backendUrl.js
+++ src/lib/constants/backendUrl.js
@@ -1,2 +1,3 @@
+/* eslint-disable no-undef */
 export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000";
 export const MIDTRANS_URL = "https://app.sandbox.midtrans.com/snap/snap.js";
