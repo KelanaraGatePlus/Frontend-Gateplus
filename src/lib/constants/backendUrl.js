1429ef33e726cde76b1cf7333d80530626f544cc
Muhammad Rakha Nasjaya <muhammadrakha3704@gmail.com>
2025-12-16 11:56:29 +0700

    [UPDATE] - Get Public backend URL from .env

--- src/lib/constants/backendUrl.js
+++ src/lib/constants/backendUrl.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://gateplussistem.online";
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000";
 export const MIDTRANS_URL = "https://app.sandbox.midtrans.com/snap/snap.js";
