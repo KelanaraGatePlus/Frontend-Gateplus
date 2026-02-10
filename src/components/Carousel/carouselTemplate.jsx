45513a2ce1980629ca5e21fb6d2141fd70ebd517
Dayrennn <raflyramandha789@gmail.com>
2026-02-10 19:16:01 +0700

    [ FIX ] - Feature Pembatasan Usia

--- src/components/Carousel/carouselTemplate.jsx
+++ src/components/Carousel/carouselTemplate.jsx
@@ -18,7 +18,6 @@ import SeriesCard from "../Card/SeriesCard";
 import PodcastCard from "../Card/PodcastCard";
 import PodcastUniqueCard from "../Card/PodcastUniqueCard";
 import EducationCard from "../Card/EducationCard";
-import Image from "next/image";
 import getMinAge from "@/lib/helper/minAge";
 
 export default function CarouselTemplate({
@@ -32,16 +31,6 @@ export default function CarouselTemplate({
   withNewestTag = false,
   isBlurred,
 }) {
-  // Default icon map untuk path lokal
-  const defaultIcons = {
-    comic: "/images/IconsContent/icons-comic.svg",
-    ebook: "/images/IconsContent/icons-ebook.svg",
-    movie: "/images/IconsContent/icons-movie.svg",
-    series: "/images/IconsContent/icons-series.svg",
-    podcast: "/images/IconsContent/icons-podcast.svg",
-    education: "/images/IconsContent/icons-education.svg",
-  };
-
   return (
     <div className="relative h-full w-full overflow-hidden rounded-md">
       <section
@@ -52,12 +41,16 @@ export default function CarouselTemplate({
         ) : (
           contents.length > 0 && (
             <section
-              className={`my-3 flex flex-col ${isOnCreatorProfile ? "my-3 md:my-0" : "my-3 md:my-5"}`}
+              className={`flex flex-col ${
+                isOnCreatorProfile ? "my-3 md:my-0" : "my-3 md:my-5"
+              }`}
             >
-              <Carousel className="sm:max-h-auto sm:max-w-auto">
+              <Carousel>
                 <p className="zeinFont mb-1 text-2xl font-extrabold text-white md:mb-2 md:text-3xl lg:text-4xl xl:text-[40px]">
                   {label}
                 </p>
+
+                {/* WRAPPER RELATIVE */}
                 <div className="relative">
                   <CarouselContent>
                     {contents.map((item, index) => {
@@ -67,73 +60,120 @@ export default function CarouselTemplate({
                           ? isBlurred(item)
                           : false;
 
-                      // Hitung minAge per item
                       const minAge = item.ageRestriction
                         ? getMinAge(item.ageRestriction)
                         : null;
+
+                      const isUniquePodcast = fixedType === "podcast" && type;
 
                       return (
                         <CarouselItem
                           key={index}
                           className={`group relative flex items-center overflow-hidden ${
                             isTopTen
-                              ? "w-[240px] sm:w-[180px] md:w-[460px]"
+                              ? "h-[190px] w-[240px] sm:h-[370px] sm:w-[180px] md:h-[370px] md:w-[460px]"
                               : "aspect-[2/3] w-[120px] sm:w-[140px] md:w-[230px]"
                           } cursor-pointer rounded-md`}
                         >
-                          {/* CARD */}
-                          <div className="relative z-0">
-                            {fixedType === "ebook" && !isTopTen && (
-                              <EbookCard
-                                withTopTag={withTopTag}
-                                title={item.title}
-                                rank={index + 1}
-                                id={item.id}
-                                coverUrl={item.posterImageUrl}
-                                hasNewEpisode={item.hasNewEpisodes}
-                                withNewestTag={withNewestTag}
-                              />
-                            )}
+                          {isTopTen ? (
+                            <div className="grid h-full w-full grid-cols-17 items-end">
+                              <div className="relative col-span-8 flex h-full w-full items-end justify-end overflow-visible">
+                                <p className="zeinFont translate-x-6 translate-y-[15%] text-[200px] leading-[0.7] font-extrabold text-[#1297DC] md:text-[310px] lg:text-[350px]">
+                                  {index + 1}
+                                </p>
+                              </div>
 
-                            {fixedType === "comic" && !isTopTen && (
-                              <ComicCard
-                                withTopTag={withTopTag}
-                                title={item.title}
-                                rank={index + 1}
-                                id={item.id}
-                                coverUrl={item.posterImageUrl}
-                                hasNewEpisode={item.hasNewEpisodes}
-                                withNewestTag={withNewestTag}
-                              />
-                            )}
+                              <div className="relative col-span-9 aspect-[2/3] overflow-hidden rounded-md">
+                                {fixedType === "ebook" && (
+                                  <EbookCard
+                                    {...item}
+                                    coverUrl={item.posterImageUrl}
+                                  />
+                                )}
+                                {fixedType === "movie" && (
+                                  <MovieCard
+                                    {...item}
+                                    coverUrl={item.thumbnailImageUrl}
+                                  />
+                                )}
+                                {fixedType === "series" && (
+                                  <SeriesCard
+                                    {...item}
+                                    coverUrl={item.thumbnailImageUrl}
+                                  />
+                                )}
+                                {fixedType === "podcast" && (
+                                  <PodcastCard
+                                    {...item}
+                                    coverUrl={item.coverPodcastImage}
+                                  />
+                                )}
+                                {fixedType === "comic" && (
+                                  <ComicCard
+                                    {...item}
+                                    coverUrl={item.posterImageUrl}
+                                  />
+                                )}
 
-                            {fixedType === "movie" && !isTopTen && (
-                              <MovieCard
-                                withTopTag={withTopTag}
-                                title={item.title}
-                                rank={index + 1}
-                                id={item.id}
-                                coverUrl={item.thumbnailImageUrl}
-                                hasNewEpisode={item.hasNewEpisodes}
-                                withNewestTag={withNewestTag}
-                              />
-                            )}
-
-                            {fixedType === "series" && !isTopTen && (
-                              <SeriesCard
-                                withTopTag={withTopTag}
-                                title={item.title}
-                                rank={index + 1}
-                                id={item.id}
-                                coverUrl={item.thumbnailImageUrl}
-                                hasNewEpisode={item.hasNewEpisodes}
-                                withNewestTag={withNewestTag}
-                              />
-                            )}
-
-                            {fixedType === "podcast" &&
-                              type == null &&
-                              !isTopTen && (
+                                {blurred && minAge && (
+                                  <div className="pointer-events-none absolute inset-0 z-10">
+                                    <div className="absolute inset-0 rounded-[6px] bg-black/10 backdrop-blur-sm" />
+                                    <div className="absolute top-1/2 left-1/2 z-20 -translate-x-1/2 -translate-y-1/2">
+                                      <span className="rounded-lg bg-red-600 px-4 py-2 text-sm font-black text-white">
+                                        {minAge}+
+                                      </span>
+                                    </div>
+                                  </div>
+                                )}
+                              </div>
+                            </div>
+                          ) : (
+                            <div className="relative h-full w-full">
+                              {fixedType === "ebook" && (
+                                <EbookCard
+                                  withTopTag={withTopTag}
+                                  title={item.title}
+                                  rank={index + 1}
+                                  id={item.id}
+                                  coverUrl={item.posterImageUrl}
+                                  hasNewEpisode={item.hasNewEpisodes}
+                                  withNewestTag={withNewestTag}
+                                />
+                              )}
+                              {fixedType === "comic" && (
+                                <ComicCard
+                                  withTopTag={withTopTag}
+                                  title={item.title}
+                                  rank={index + 1}
+                                  id={item.id}
+                                  coverUrl={item.posterImageUrl}
+                                  hasNewEpisode={item.hasNewEpisodes}
+                                  withNewestTag={withNewestTag}
+                                />
+                              )}
+                              {fixedType === "movie" && (
+                                <MovieCard
+                                  withTopTag={withTopTag}
+                                  title={item.title}
+                                  rank={index + 1}
+                                  id={item.id}
+                                  coverUrl={item.thumbnailImageUrl}
+                                  hasNewEpisode={item.hasNewEpisodes}
+                                  withNewestTag={withNewestTag}
+                                />
+                              )}
+                              {fixedType === "series" && (
+                                <SeriesCard
+                                  withTopTag={withTopTag}
+                                  title={item.title}
+                                  rank={index + 1}
+                                  id={item.id}
+                                  coverUrl={item.thumbnailImageUrl}
+                                  hasNewEpisode={item.hasNewEpisodes}
+                                  withNewestTag={withNewestTag}
+                                />
+                              )}
+                              {fixedType === "podcast" && !type && (
                                 <PodcastCard
                                   withTopTag={withTopTag}
                                   title={item.title}
@@ -145,76 +185,37 @@ export default function CarouselTemplate({
                                 />
                               )}
 
-                            {fixedType === "podcast" && type && !isTopTen && (
-                              <PodcastUniqueCard
-                                title={item.title}
-                                id={item.id}
-                                coverUrl={item.coverPodcastImage}
-                                creatorName={item.Creator?.profileName}
-                                releaseDate={item.createdAt}
-                                hasNewEpisode={item.hasNewEpisodes}
-                              />
-                            )}
+                              {isUniquePodcast && (
+                                <PodcastUniqueCard
+                                  title={item.title}
+                                  id={item.id}
+                                  coverUrl={item.coverPodcastImage}
+                                  creatorName={item.Creator?.profileName}
+                                  releaseDate={item.createdAt}
+                                  hasNewEpisode={item.hasNewEpisodes}
+                                  isBlurred={blurred}
+                                  minAge={minAge}
+                                />
+                              )}
 
-                            {fixedType === "education" && !isTopTen && (
-                              <EducationCard
-                                title={item.title}
-                                id={item.id}
-                                coverUrl={item.bannerUrl}
-                                creatorName={item.creator?.profileName}
-                                releaseDate={item.createdAt}
-                              />
-                            )}
+                              {fixedType === "education" && (
+                                <EducationCard
+                                  title={item.title}
+                                  id={item.id}
+                                  coverUrl={item.bannerUrl}
+                                  creatorName={item.creator?.profileName}
+                                  releaseDate={item.createdAt}
+                                />
+                              )}
 
-                            {/* TOP 10 */}
-                            {isTopTen && (
-                              <div className="grid grid-cols-17">
-                                <div className="relative col-span-8 h-full w-full">
-                                  <p className="zeinFont absolute -right-4 -bottom-10 -mb-4 flex text-[230px] leading-none font-extrabold text-[#1297DC] md:-right-6 md:bottom-5 md:-mb-8 md:text-[350px]">
-                                    {index + 1}
-                                  </p>
-                                </div>
-                                <div className="col-span-9 aspect-[2/3] overflow-hidden rounded-md">
-                                  {fixedType === "ebook" && (
-                                    <EbookCard
-                                      {...item}
-                                      coverUrl={item.posterImageUrl}
-                                    />
-                                  )}
-                                  {fixedType === "movie" && (
-                                    <MovieCard
-                                      {...item}
-                                      coverUrl={item.thumbnailImageUrl}
-                                    />
-                                  )}
-                                </div>
-                              </div>
-                            )}
-                          </div>
-
-                          {/* BLUR OVERLAY */}
-                          {blurred && minAge && (
-                            <div className="pointer-events-none absolute inset-0 z-10">
-                              {/* overlay blur */}
-                              <div className="absolute inset-0 bg-black/10 backdrop-blur-sm" />
-                              {/* age restriction tag */}
-                              <div className="pointer-events-auto absolute top-1/2 left-1/2 z-20 -translate-x-1/2 -translate-y-1/2">
-                                <span className="rounded-lg bg-red-600 px-4 py-2 text-sm font-black text-white shadow-sm">
-                                  {minAge}+
-                                </span>
-                              </div>
-                              S{/* icon kecil */}
-                              {(item.iconUrl || defaultIcons[fixedType]) && (
-                                <div className="pointer-events-auto absolute bottom-2 left-2 z-30 h-9 w-15">
-                                  <Image
-                                    src={
-                                      item.iconUrl || defaultIcons[fixedType]
-                                    }
-                                    alt={`${fixedType} Icon`}
-                                    width={45}
-                                    height={45}
-                                    className="object-contain"
-                                  />
+                              {blurred && minAge && !isUniquePodcast && (
+                                <div className="pointer-events-none absolute inset-0 z-10">
+                                  <div className="absolute inset-0 rounded-[6px] bg-black/10 backdrop-blur-sm" />
+                                  <div className="absolute top-1/2 left-1/2 z-20 -translate-x-1/2 -translate-y-1/2">
+                                    <span className="rounded-lg bg-red-600 px-4 py-2 text-sm font-black text-white">
+                                      {minAge}+
+                                    </span>
+                                  </div>
                                 </div>
                               )}
                             </div>
@@ -223,6 +224,14 @@ export default function CarouselTemplate({
                       );
                     })}
                   </CarouselContent>
+                  {type !== "podcast" && (
+                    <>
+                      <div className="pointer-events-none absolute top-0 left-0 z-10 h-full w-10 rounded-[6px] bg-gradient-to-r from-white/20 to-transparent backdrop-blur-[0.5px]" />
+                      <div className="pointer-events-none absolute top-0 right-0 z-10 h-full w-10 rounded-[6px] bg-gradient-to-l from-white/20 to-transparent backdrop-blur-[0.5px]" />
+                    </>
+                  )}
+
+                  {/* BUTTON */}
                   <CarouselPrevious />
                   <CarouselNext />
                 </div>
@@ -242,7 +251,6 @@ CarouselTemplate.propTypes = {
   isLoading: PropTypes.bool.isRequired,
   isTopTen: PropTypes.bool,
   isOnCreatorProfile: PropTypes.bool,
-  withGradient: PropTypes.bool,
   withTopTag: PropTypes.bool,
   withNewestTag: PropTypes.bool,
   isBlurred: PropTypes.func,
