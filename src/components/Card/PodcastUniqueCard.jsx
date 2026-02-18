fad2d0ac7c77a1e8cccd02a338989c1ae21899c3
Muhammad Rakha Nasjaya <muhammadrakha3704@gmail.com>
2026-02-18 16:08:18 +0700

    [FIX] - EsLint

--- src/components/Card/PodcastUniqueCard.jsx
+++ src/components/Card/PodcastUniqueCard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { contentType } from "@/lib/constants/contentType";
-import Image from "next/image";
 import Link from "next/link";
 import PropTypes from "prop-types";
 
